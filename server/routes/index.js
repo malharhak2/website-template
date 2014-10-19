9a233e82a2985739f3cfeef15b710feaf75ee96c
Anthony Pigeot <anthony.pigeot@gmail.com>
2014-10-19 12:49:11 +0200

    Added example file in js folder for example

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+var exampleScript = require('../js/exampleScript.js');
 /* GET home page. */
 router.get('/', function(req, res) {
+	exampleScript();
   res.render('index', { title: 'Index' });
 });
 
